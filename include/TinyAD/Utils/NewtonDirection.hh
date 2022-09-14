54107dc1f214890c26b397477ff6f8990fde8184
David Jourdan <david.jourdan@inria.fr>
2022-09-14 10:56:04 +0200

    use custom solver in newton_direction

--- include/TinyAD/Utils/NewtonDirection.hh
+++ include/TinyAD/Utils/NewtonDirection.hh
@@ -22,11 +22,11 @@ namespace TinyAD
  *      _solver: A solver that can be used over multiple iterations
  *               in case the sparsity pattern of _H_proj is constant.
  */
-template <typename PassiveT>
+template <typename PassiveT, typename SolverT>
 Eigen::VectorX<PassiveT> newton_direction(
         const Eigen::VectorX<PassiveT>& _g,
         const Eigen::SparseMatrix<PassiveT>& _H_proj,
-        LinearSolver<PassiveT>& _solver,
+        LinearSolver<PassiveT, SolverT>& _solver,
         const PassiveT& _w_identity = 0.0)
 {
     const Eigen::SparseMatrix<PassiveT> H_reg = _w_identity * identity<PassiveT>(_g.size()) + _H_proj;
